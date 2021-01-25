27f251ef830e5e41dcfb3dd1e8dbc3070f0fe0ac
Arif Yilmaz <arifyznl@gmail.com>
2021-01-25 22:02:01 +0100

    updated

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(name='pyRDF2Vec',
       version='0.0.5',
       description='A python implementation of RDF2Vec',
-      authors='Gilles Vandewiele, Bram Steenwinckel, Michael Weyns',
+      authors='Arif Yilmaz, Gilles Vandewiele, Bram Steenwinckel, Michael Weyns',
       author_email='gilles.vandewiele@ugent.be',
-      url='https://github.com/IBCNServices/pyRDF2Vec',
+      url='https://github.com/arifx/pyRDF2Vec',
       packages=['rdf2vec', 'rdf2vec/walkers'],
       install_requires=['gensim', 'matplotlib', 'networkx', 'numpy', 
           'pandas', 'rdflib', 'scikit_learn', 'tqdm', 'python-louvain'
